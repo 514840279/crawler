16d65e01bd12e28bd05cc535b1bdd153781bc4ad
514840279 <514840279@qq.com>
2019-05-22 17:47:01 +0800

    软件启动图

--- code/RunWindows.py
+++ code/RunWindows.py
@@ -2,14 +2,15 @@
 
 import sys
 from Windows import Ui_MainWindow
-from PyQt5 import QtWidgets
-from PyQt5.QtGui import QStandardItemModel,QStandardItem
-from PyQt5.QtWidgets import QHBoxLayout,QWidget,QPushButton,QTableWidgetItem
+from PyQt5 import QtGui,QtCore,QtWidgets
+from PyQt5.QtGui import QStandardItemModel,QStandardItem,QFont
+from PyQt5.QtWidgets import QHBoxLayout,QWidget,QPushButton,QTableWidgetItem,QStyle
 from common.HtmlSource import HtmlSource
 from common.RuleConf import Rule
 from lxml import html
 
 import uuid
+import time
 
 
 
@@ -245,6 +246,37 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     ui =  BtnLinsenWindow()
 
+    # 创建QSplashScreen对象实例
+    splash = QtWidgets.QSplashScreen(QtGui.QPixmap("uugai_94x194.png"))
+    # 设置画面中的文字的字体
+    splash.setFont(QFont('Microsoft YaHei UI', 12))
+    #splash.setStyleSheet("#MainWindow{background-color: green}")
+    # 显示画面
+    splash.show()
+    # 显示信息
+    splash.showMessage("启动中... 0%", QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom, QtCore.Qt.white)
+    time.sleep(1)
+    splash.showMessage("正在加载样式表...20%", QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom, QtCore.Qt.white)
+    #if (ui_style == 'dark'):
+    #    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    time.sleep(1)
+    splash.showMessage("正在加载样式表...40%", QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom, QtCore.Qt.white)
+    time.sleep(1)
+    splash.showMessage("正在加载数据库配置...60%", QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom, QtCore.Qt.white)
+    time.sleep(1)
+    splash.showMessage("正在测试数据库连接...80%", QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom, QtCore.Qt.white)
+    #conn = GetMysqlConnection().get_connection()
+    #if (conn != 0):
+    #    conn.close()
+     #   splash.showMessage("正在测试数据库连接...Success", QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom, QtCore.Qt.white)
+    #else:
+    #    splash.showMessage("正在测试数据库连接...faild", QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom, QtCore.Qt.white)
+    time.sleep(2)
+    splash.showMessage("启动中...100%", QtCore.Qt.AlignLeft | QtCore.Qt.AlignBottom, QtCore.Qt.white)
+    time.sleep(1)
     ui.show()
+    # 当主界面显示后销毁启动画面
+    splash.finish(ui)
+
     sys.exit(app.exec_())
 

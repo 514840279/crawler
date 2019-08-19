34ac592d5fa514ee036802348e678e5052ea9482
514840279 <514840279@qq.com>
2019-08-19 16:26:22 +0800

    .

--- common/ResultData.py
+++ common/ResultData.py
@@ -3,47 +3,52 @@
 # develop 514840279
 
 from common.inc_conn import *
+from common.Mysql_Utils import MyPymysqlPool
 import re
-resultDb = Conn_mysql(db="application") # 结果入库
+
+resultDb = Conn_mysql(db="application")  # 结果入库
+
 
 class ResultData:
 
     # 检查映射配置信息
-    def resultRefulence(self,rule_uuid,result,type):
+    def resultRefulence(self, rule_uuid, result, type):
         # 拼接查询映射关系表语句
-        sql = "SELECT distinct  `table_uuid`FROM `application`.`sys_seed_result_ruler_info`WHERE ruler_uuid= '%s' "%(rule_uuid)
-        re,data = resultDb.read_sql(sql=sql)
+        sql = "SELECT distinct  `table_uuid`FROM `application`.`sys_seed_result_ruler_info`WHERE ruler_uuid= '%s' " % (
+            rule_uuid)
+        re, data = resultDb.read_sql(sql=sql)
         # 检查有多少配置
         for table_uuid in data:
-            self.writeToMysql(table_uuid[0],result,type)
+            self.writeToMysql(table_uuid[0], result, type)
+
     # 数据库入库
-    def writeToMysql(self,table_uuid,result,type):
+    def writeToMysql(self, table_uuid, result, type):
         # 查询入库的表名
-        sql = "SELECT   `table_name` FROM  `application`.`sys_table_info` WHERE UUID = '%s'" %table_uuid
+        sql = "SELECT   `table_name` FROM  `application`.`sys_table_info` WHERE UUID = '%s'" % table_uuid
         redx, table_name = resultDb.read_sql(sql=sql)
         # 查询出完整的字段映射对应关系
-        sql = "SELECT  `cols_name`,    `ruler_colum_name` FROM `application`.`sys_seed_result_ruler_info` WHERE table_uuid = '%s'"%table_uuid
+        sql = "SELECT  `cols_name`,    `ruler_colum_name` FROM `application`.`sys_seed_result_ruler_info` WHERE table_uuid = '%s'" % table_uuid
         redx, data = resultDb.read_sql(sql=sql)
         # 拼接入库语句
-        #sql =" insert into %s (%s) values(%s)"
-        col=""
-        for i in  range(len(data)):
-            if(i >0):
-                col = col +","
+        # sql =" insert into %s (%s) values(%s)"
+        col = ""
+        for i in range(len(data)):
+            if (i > 0):
+                col = col + ","
             col = col + data[i][0]
 
         if type == 'list':
             print("列表模板信息入库！")
             # 读取映射信息
-            for info in  result:
+            for info in result:
                 info = self.tupleDataToDic(info)
                 value = ""
-                for i in  range(len(data)):
+                for i in range(len(data)):
                     if (i > 0):
                         value = value + ","
-                    value = value + "'"+ re.sub(r"[','\"\]\[]","",str(info[data[i][0]])).strip() +"'"
+                    value = value + "'" + re.sub(r"[','\"\]\[]", "", str(info[data[i][0]])).strip() + "'"
                 # 拼接入库语句
-                sql = "insert into %s (%s) values(%s)"%(table_name[0][0],col,value)
+                sql = "insert into %s (%s) values(%s)" % (table_name[0][0], col, value)
                 resultDb.write_sql(sql=sql)
         else:
             print("详细模板信息入库！")
@@ -52,20 +57,40 @@ class ResultData:
             for i in range(len(data)):
                 if (i > 0):
                     value = value + ","
-                value = value + "'" + re.sub(r"[','\"\]\[]","",str(info[data[i][0]])).strip() + "'"
+                value = value + "'" + re.sub(r"[','\"\]\[]", "", str(info[data[i][0]])).strip() + "'"
             # 拼接入库语句
             sql = "insert into %s (%s) values(%s)" % (table_name[0][0], col, value)
             resultDb.write_sql(sql=sql)
 
-    def tupleDataToDic(self,info):
-        dic ={}
+    def tupleDataToDic(self, info):
+        dic = {}
         for i in info:
-            dic[i[0]]=i[1]
+            dic[i[0]] = i[1]
         return dic
 
-    def main(self):
-        print("")
+    if __name__ == '__main__':
+        pass
+
+
+class SysCrawlerTaskInfo:
+
+    def __init__(self):
+        self.dbpool = MyPymysqlPool("default")
+
+    def findAll(self):
+        sql = "select * from sys_crawler_task_info t"
+        return self.dbpool.getAll(sql)
+
+    def updateDelete(self,id,val):
+        sql = " update sys_crawler_task_info set delete_flag = %d where uuid='%s'" %(val,id)
+        return self.dbpool.update(sql)
+
+    def __del__(self):
+        self.dbpool.dispose()
+
+    if __name__ == '__main__':
+        pass
 
 
 if __name__ == '__main__':
-    print("")
+    pass
--- ui/RunTaskListen.py
+++ ui/RunTaskListen.py
@@ -1,9 +1,139 @@
 from PyQt5.QtWidgets import QWidget
 
 from ui.taskListen import  Ui_Form
+from common.ResultData import SysCrawlerTaskInfo
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QHBoxLayout,QWidget,QPushButton,QTableWidgetItem, QLabel, QVBoxLayout
+import datetime
+import requests,time
 
 
 class RunTaskListen(QWidget, Ui_Form):
     def __init__(self):
         super(RunTaskListen, self).__init__()
         self.setupUi(self)
+        self.showTables()
+
+    def showTables(self):
+
+        self.tableWidget.setVisible(True)
+        sysCrawlerTaskInfo = SysCrawlerTaskInfo()
+        taskListData = sysCrawlerTaskInfo.findAll()
+        if taskListData != False:
+            #title = list(taskListData[0].keys())
+            title = ['操作','图标','task_name',
+                     #'url',
+                     'url_type','url_name','discription',
+                     'charset','request_data','request_type','start_time',
+                     'excute_batch',
+                     'create_time','create_user',
+                     'update_time','update_user',
+                     'excute_flag','home_statue','dict_conf','list_conf','detail_conf'
+                     ]
+
+            # 设置数据层次结构，4行4列
+            self.tableWidget.setRowCount(99)  # 行下标最大值
+            self.tableWidget.setColumnCount(len(title))  # 列
+
+            # 设置水平方向四个头标签文本内容
+            self.tableWidget.setHorizontalHeaderLabels(title)
+
+            for row in range(len(taskListData)):
+                if taskListData[row]['delete_flag'] is not 1:
+                    button = self.buttonForDeleteTrue(id=taskListData[row]['uuid'])
+                    # 设置每个位置的按钮
+                    self.tableWidget.setCellWidget(row, 0, button)
+                else:
+                    button = self.buttonForDeleteFalse(id=taskListData[row]['uuid'])
+                    # 设置每个位置的按钮
+                    self.tableWidget.setCellWidget(row, 0, button)
+
+                if taskListData[row]['web_icon'] is not None:
+                    img = self.showImg(url=taskListData[row]['web_icon'] )
+                    # 设置每个位置的按钮
+                    self.tableWidget.setCellWidget(row, 1, img)
+                else:
+                    # 设置每个位置的按钮
+                    self.tableWidget.setCellWidget(row, 1, None)
+
+                for column in range(len(title)-2):
+                    print(taskListData[row])
+                    data = taskListData[row][title[column+2]]
+                    if isinstance(data, datetime.datetime):
+                        date = data.strftime("%Y-%m-%d %H:%M:%S")
+                        item = QTableWidgetItem(date)
+                        # 设置每个位置的文本值
+                        self.tableWidget.setItem(row, column+2, item)
+                    else:
+                        item = QTableWidgetItem(data)
+                        # 设置每个位置的文本值
+                        self.tableWidget.setItem(row, column+2, item)
+        del sysCrawlerTaskInfo
+
+
+    # 列表内添加按钮
+    def buttonForDeleteTrue(self, id):
+
+        widget = QWidget()
+        # 修改
+        updateBtn = QPushButton('已启用')
+        updateBtn.setStyleSheet(''' text-align : center;
+                                          background-color : NavajoWhite;
+                                          height : 30px;
+                                          border-style: outset;
+                                          font : 13px  ''')
+        updateBtn.clicked.connect(lambda: self.updateDeleteFalse(id))
+
+        hLayout = QHBoxLayout()
+        hLayout.addWidget(updateBtn)
+        hLayout.setContentsMargins(5, 2, 5, 2)
+        widget.setLayout(hLayout)
+        return widget
+
+    def updateDeleteFalse(self,id):
+        sysCrawlerTaskInfo = SysCrawlerTaskInfo()
+        sysCrawlerTaskInfo.updateDelete(id,1)
+        del sysCrawlerTaskInfo
+        self.showTables()
+
+    # 列表内添加按钮
+    def buttonForDeleteFalse(self, id):
+        widget = QWidget()
+
+        # 删除
+        deleteBtn = QPushButton('已废弃')
+        deleteBtn.setStyleSheet(''' text-align : center;
+                                    background-color : LightCoral;
+                                    height : 30px;
+                                    border-style: outset;
+                                    font : 13px; ''')
+        deleteBtn.clicked.connect(lambda: self.updateDeleteTrue(id))
+        hLayout = QHBoxLayout()
+        hLayout.addWidget(deleteBtn)
+        hLayout.setContentsMargins(5, 2, 5, 2)
+        widget.setLayout(hLayout)
+        return widget
+
+    def updateDeleteTrue(self,id):
+        sysCrawlerTaskInfo = SysCrawlerTaskInfo()
+        sysCrawlerTaskInfo.updateDelete(id,0)
+        del sysCrawlerTaskInfo
+        self.showTables()
+
+    def showImg(self,url):
+        req = requests.get(url)
+        photo = QPixmap()
+
+        photo.loadFromData(req.content)
+
+        label = QLabel()
+        label.setPixmap(photo)
+
+        widget = QWidget()
+        layout = QVBoxLayout()
+        widget.setLayout(layout)
+        layout.addWidget(label)
+
+        widget.show()
+        return widget
